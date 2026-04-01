2d5a136c5c6491b1180f1660fbf17ef3ef136bd1
perturbatorandtonny <perturbatorandtonny@gmail.com>
2026-04-01 12:53:29 -0500

    feat(session): add type for payload data return

--- src/utils/session.ts
+++ src/utils/session.ts
@@ -1,7 +1,12 @@
 import jwt from 'jsonwebtoken';
 
+//mover el type jwtPayload a un achivo de tipados
+export type jwtPayload = {
+  role:string
+}
+
 export  function createSession(userRole: string): string {
-  const payload = {
+  const payload: jwtPayload = {
     role: userRole,
   }
   const token = jwt.sign(payload, "secretHere", {
