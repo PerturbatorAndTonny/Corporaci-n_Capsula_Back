74256b68e657af9214959243b80c15a6ff8d9010
perturbatorandtonny <perturbatorandtonny@gmail.com>
2026-04-03 15:54:59 -0500

    fix(session): implement all validations for login resource

--- src/controllers/session.ts
+++ src/controllers/session.ts
@@ -1,26 +1,39 @@
 import type { Request, Response } from "express";
 import { createSession } from "../utils/session.js";
-// import type { AuthInput } from "../schemas/auth.js";
+import type { AuthInput } from "../schemas/auth.js";
+import { usersDB } from "../models/modelUser.js";
+
+import { comparePass } from "../utils/pass.js";
 
 export const newSession = async (req: Request, res: Response) => {
   try {
-    /*
-    Aquí iría la lógica para verificar las credenciales del usuario
+    // oxlint-disable-next-line no-unused-vars
     const { userId, password, authHash } = req.body as AuthInput;
-    -
-    -
-    -
-    -
-    -
-    -
 
-    */
+    const isUserExist = usersDB.find(
+      (User) => User.id === Number(userId)
+    )
 
-    const token = await createSession({ role: "Administrador" })
+    if (!isUserExist) {
+      return res.status(404).json({
+        messageError: "user not found"
+      })
+    }
+
+    const isSame = await comparePass(password, isUserExist.pass)
+
+    if (!isSame) {
+      isUserExist.failed_attempts++
+      return res.status(401).json({
+        messageError: "Invalid credentials, try again"
+      })
+    }
+
+    const token = await createSession({ role: isUserExist.idrol })
 
     return res.status(200).cookie("token", token).json({
       message: "Session created successfully",
-      data: "Administrador",
+      data: isUserExist.idrol,
       session: token
     })
 
