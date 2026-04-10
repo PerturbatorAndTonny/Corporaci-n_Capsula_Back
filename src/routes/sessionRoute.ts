3bf713cb5985566ca12a72dc24f275c5163bf976
perturbatorandtonny <perturbatorandtonny@gmail.com>
2026-04-10 11:48:58 -0500

    fix(session): add middlewares for validate and verify jwt token

--- src/routes/sessionRoute.ts
+++ src/routes/sessionRoute.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { newSession } from "../controllers/sessionController.js";
+import { newSession, closeSession } from "../controllers/sessionController.js";
 import { verifyData } from "../middlewares/verifyData.js"
+import { verifyToken } from '../middlewares/verifyToken.js'
 import { authSchema } from "../schemas/authSchema.js";
 
 const router: Router = Router();
 
 router.post("/auth/login", verifyData(authSchema), newSession);
+router.delete("/auth/logout", verifyToken, closeSession);
 
 export default router;
