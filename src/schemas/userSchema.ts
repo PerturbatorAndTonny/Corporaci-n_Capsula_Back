23d0d84a1f20d5a2695ed87c81149747f6cb8e26
Rodri <rodrigo.munoza@konradlorenz.edu.co>
2026-04-10 23:04:09 -0500

    userSchema update

--- src/schemas/userSchema.ts
+++ src/schemas/userSchema.ts
@@ -1,26 +1,21 @@
 import { z } from 'zod';
 import { roleEnum } from './roleSchema.js';
 
-export const AuthTypeEnum = z.enum(["FACIAL", "DNA_HUMAN"], {
-    error: "El tipo de auth debe ser FACIAL o DNA_HUMAN"
-});
 
 export const schemaUserCreate = z.object({
-    name: z.string({
+    nombre: z.string({
         error: "El nombre es requerido",
     }).min(1, "El nombre no puede estar vacío"),
 
-    age: z.number({
+    edad: z.number({
         error: "La edad es requerida",
     }).int("La edad debe ser un número entero").positive("La edad debe ser un número positivo"),
 
-    idrol: roleEnum,
 
-    pass: z.string({
+    contraseña: z.string({
         error: "La contraseña es requerida",
     }).min(8, "La contraseña debe tener al menos 8 caracteres"),
 
-    authType: AuthTypeEnum
 });
 
 export type UserCreate = z.infer<typeof schemaUserCreate>;
