84ca96572c518a26f45fdee68549c1e8527f9319
perturbatorandtonny <perturbatorandtonny@gmail.com>
2026-04-11 18:16:47 -0500

    feat(role)!: implement function conectors to DB server and change actual role

--- src/controllers/roleController.ts
+++ src/controllers/roleController.ts
@@ -1,28 +1,28 @@
 import type { Request, Response } from "express";
 import type { rolType } from "../schemas/roleSchema.js";
 
-import { usersDB } from "../models/userModel.js";
+import { userExist, modifyUserRol } from "../models/userModel.js"
 
-export const updateUserRole = (req: Request, res: Response) => {
+export const updateUserRole = async (req: Request, res: Response) => {
   try {
     const { id } = req.params;
     const { role } = req.body as rolType;
 
-    const isUserExist = usersDB.find(
-      (User) => User.id === Number(id)
-    )
+    const isUserExist = await userExist(Number(id));
 
     if (!isUserExist) {
       return res.status(404).json({
-        messageError: "user not found"
+        message: "user not found"
       })
     }
 
-    isUserExist.idrol = role;
-
+    const userRolUpdate = await modifyUserRol(Number(id), role)
+    
     return res.status(200).json({
-      message: `User with id ${id} has been updated to role ${role}`
+      message: `user updated`,
+      data: userRolUpdate
     })
+
   } catch (error) {
     return res.status(500).json({
       message: error,
