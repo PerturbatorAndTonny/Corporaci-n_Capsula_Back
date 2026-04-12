8ba3201237312d555284b2d6b4b72a081d0c45d8
perturbatorandtonny <perturbatorandtonny@gmail.com>
2026-04-12 11:01:06 -0500

    feat(routes)!: implement validation for token and rol

--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -2,13 +2,14 @@ import { Router } from 'express';
 import { createUser, getUsers, getUserById, updateUserById, deleteUserById } from '../controllers/userController.js';
 import { schemaUserCreate, schemaUserUpdate } from '../schemas/userSchema.js';
 import { verifyData } from '../middlewares/verifyData.js';
+import { verifyRole, verifyToken } from "../middlewares/verifyToken.js"
 
 const router = Router();
 
-router.post('/user', verifyData(schemaUserCreate), createUser);
-router.get('/user', getUsers);
-router.get('/user/:id', getUserById);
-router.patch('/user/:id', verifyData(schemaUserUpdate), updateUserById);
-router.delete('/user/:id', deleteUserById);
+router.post('/user', verifyToken, verifyRole("Administrador"), verifyData(schemaUserCreate), createUser);
+router.get('/user', verifyToken, verifyRole("Administrador"), getUsers);
+router.get('/user/:id', verifyToken, verifyRole("Administrador"), getUserById);
+router.patch('/user/:id', verifyToken, verifyRole("Administrador"), verifyData(schemaUserUpdate), updateUserById);
+router.delete('/user/:id',verifyToken, verifyRole("Administrador"), deleteUserById);
 
 export default router;
