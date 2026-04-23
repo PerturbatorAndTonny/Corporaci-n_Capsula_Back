1b751d7d3359114563d4f429bce819866d20cf1e
perturbatorandtonny <perturbatorandtonny@gmail.com>
2026-04-23 11:41:15 -0500

    feat(role): implement route for get all user by rol petition

--- src/routes/roleRoute.ts
+++ src/routes/roleRoute.ts
@@ -1,11 +1,12 @@
 import { Router } from "express"
-import { updateUserRole } from "../controllers/roleController.js";
+import { getUsersByRol, updateUserRole } from "../controllers/roleController.js";
 import { verifyData } from '../middlewares/verifyData.js'
 import { rolSchema } from '../schemas/roleSchema.js'
 import { verifyRole, verifyToken } from "../middlewares/verifyToken.js"
 
 const router: Router = Router();
 
-router.patch("/users/:id/role", verifyToken, verifyRole("Administrador"), verifyData(rolSchema), updateUserRole)
+router.get("/users/roles", getUsersByRol)
+router.patch("/users/:id/role", verifyToken, verifyRole("Administrador"), verifyData(rolSchema), updateUserRole);
 
 export default router; 
