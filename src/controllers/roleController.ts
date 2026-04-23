141152d1c855f009a8043de061c06d1527639175
perturbatorandtonny <perturbatorandtonny@gmail.com>
2026-04-23 11:40:41 -0500

    feat(role): implement controller for handling get user by rol

--- src/controllers/roleController.ts
+++ src/controllers/roleController.ts
@@ -2,6 +2,7 @@ import type { Request, Response } from "express";
 import type { rolType } from "../schemas/roleSchema.js";
 
 import { userExist, modifyUserRol } from "../models/userModel.js"
+import { getAlluserByRol } from '../models/roleModel.js'
 
 export const updateUserRole = async (req: Request, res: Response) => {
   try {
@@ -17,12 +18,26 @@ export const updateUserRole = async (req: Request, res: Response) => {
     }
 
     const userRolUpdate = await modifyUserRol(Number(id), role)
-    
+
     return res.status(200).json({
       message: `user updated`,
       data: userRolUpdate
     })
 
+  } catch (error) {
+    return res.status(500).json({
+      message: error,
+    })
+  }
+}
+
+export const getUsersByRol = async (req: Request, res: Response) => {
+  try {
+    const users = await getAlluserByRol();
+    return res.status(200).json({
+      message: 'List of users by rol',
+      data: users
+    })
   } catch (error) {
     return res.status(500).json({
       message: error,
