2cd5edc864accb78891540737577a8a788672781
perturbatorandtonny <perturbatorandtonny@gmail.com>
2026-04-23 11:48:00 -0500

    fix(role): implement interface for better usability

--- src/models/roleModel.ts
+++ src/models/roleModel.ts
@@ -1,10 +1,18 @@
 import { sql } from '../utils/conection.js'
 
-export async function getAlluserByRol(){
+interface userByRol {
+  id_usuario: number,
+  nombre: string,
+  id_rol: number,
+  nombre_rol: string,
+  nivel_seguridad: string
+}
+
+export async function getAlluserByRol(): Promise<userByRol[]>{
   const result = await sql`
   SELECT u.id_usuario, u.nombre, r.id_rol as rol, r.nombre_rol, r.nivel_seguridad 
   FROM usuario u 
   INNER JOIN rol r on u.id_rol = r.id_rol 
   ORDER BY u.id_rol ASC`
-  return result
+  return result as userByRol[]
 }
