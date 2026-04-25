6c0e7ad2fb6a4291bfb61ee654a642b29453228e
JZHPX <176835358+JZHPX@users.noreply.github.com>
2026-04-25 13:27:12 -0500

    feat(session): added account blocking and verification

--- src/models/sessionModel.ts
+++ src/models/sessionModel.ts
@@ -5,11 +5,19 @@ interface userInfo {
   nombre: string
 }
 
-export async function getUserCredentials(nameUser: string): Promise<{
-  password: string
-}> {
-  const result = await sql`select u."password"  from usuario as u where u.nombre = ${nameUser}`
-  return result[0] as { password: string }
+type UserCredentials = {
+  password: string;
+  estado: boolean;
+}
+
+export async function getUserCredentials(nameUser: string): Promise<UserCredentials | null>{
+  const result = await sql`select u."password", u."estado" from usuario as u where u.nombre = ${nameUser}`
+  const row = result[0];
+  if (!row) return null;
+  return {
+    password: row.password,
+    estado: row.estado,
+  }
 }
 
 export async function getUserRole(nameUser: string): Promise<userInfo> {
--- src/utils/session.ts
+++ src/utils/session.ts
@@ -22,3 +22,66 @@ export function addToBlacklist(token: string): void {
 export function isBlacklisted(token: string): boolean {
   return blackList.has(token)
 }
+
+//Intentos sesion
+
+interface AttemptInfo {
+  attempts: number;
+  blockedUntil: number | null;
+}
+
+
+const attemptsMap = new Map<string, AttemptInfo>();
+
+const MAX_ATTEMPTS = 5;
+const BLOCK_TIME = 15 * 60 * 1000; // 15 min
+
+export function getAttemptInfo(user: string): AttemptInfo {
+  return attemptsMap.get(user) ?? { attempts: 0, blockedUntil: null };
+}
+
+export function isBlocked(user: string): { blocked: boolean; remaining: number } {
+  const info = getAttemptInfo(user);
+
+  if (!info.blockedUntil) {
+    return { blocked: false, remaining: 0 };
+  }
+
+  const remaining = info.blockedUntil - Date.now();
+
+  if (remaining <= 0) { //Al pasar 15 min el usuario tiene otras 5 oportunidades
+    attemptsMap.delete(user);
+    return { blocked: false, remaining: 0 };
+  }
+
+  return { blocked: true, remaining };
+}
+
+export function registerFailedAttempt(user: string): AttemptInfo {
+  const current = getAttemptInfo(user);
+  const attempts = current.attempts + 1;
+
+  if (attempts >= MAX_ATTEMPTS) {
+    const blockedUntil = Date.now() + BLOCK_TIME;
+
+    const data: AttemptInfo = {
+      attempts,
+      blockedUntil
+    };
+
+    attemptsMap.set(user, data);
+    return data;
+  }
+
+  const data: AttemptInfo = {
+    attempts,
+    blockedUntil: null
+  };
+
+  attemptsMap.set(user, data);
+  return data;
+}
+
+export function resetAttempts(user: string): void {
+  attemptsMap.delete(user);
+}
