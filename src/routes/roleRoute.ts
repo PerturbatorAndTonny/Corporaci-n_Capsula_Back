0741d9eddaa546e22262b0fdfec760b754fa9e04
JZHPX <176835358+JZHPX@users.noreply.github.com>
2026-04-26 20:09:06 -0500

    feat/ added incidents middleware to the routes at src/routes

--- src/routes/roleRoute.ts
+++ src/routes/roleRoute.ts
@@ -3,9 +3,10 @@ import { updateUserRole } from "../controllers/roleController.js";
 import { verifyData } from '../middlewares/verifyData.js'
 import { rolSchema } from '../schemas/roleSchema.js'
 import { verifyRole, verifyToken } from "../middlewares/verifyToken.js"
+import { incidenteMiddleware } from "../middlewares/incident.js";
 
 const router: Router = Router();
 
-router.patch("/users/:id/role", verifyToken, verifyRole("Administrador"), verifyData(rolSchema), updateUserRole)
+router.patch("/users/:id/role", verifyToken, verifyRole("Administrador"), verifyData(rolSchema), incidenteMiddleware, updateUserRole)
 
 export default router; 
--- src/routes/userRoute.ts
+++ src/routes/userRoute.ts
@@ -3,13 +3,14 @@ import { createUser, getUsers, getUserById, updateUserById, deleteUserById } fro
 import { schemaUserCreate, schemaUserUpdate } from '../schemas/userSchema.js';
 import { verifyData } from '../middlewares/verifyData.js';
 import { verifyRole, verifyToken } from "../middlewares/verifyToken.js"
+import { incidenteMiddleware } from '../middlewares/incident.js';
 
 const router = Router();
 
-router.post('/user', verifyToken, verifyRole("Administrador"), verifyData(schemaUserCreate), createUser);
-router.get('/user', verifyToken, verifyRole("Administrador"), getUsers);
-router.get('/user/:id', verifyToken, verifyRole("Administrador"), getUserById);
-router.patch('/user/:id', verifyToken, verifyRole("Administrador"), verifyData(schemaUserUpdate), updateUserById);
-router.delete('/user/:id',verifyToken, verifyRole("Administrador"), deleteUserById);
+router.post('/user', verifyToken, verifyRole("Administrador"), verifyData(schemaUserCreate), incidenteMiddleware, createUser);
+router.get('/user', verifyToken, verifyRole("Administrador"), incidenteMiddleware, getUsers);
+router.get('/user/:id', verifyToken, verifyRole("Administrador"), incidenteMiddleware, getUserById);
+router.patch('/user/:id', verifyToken, verifyRole("Administrador"),  verifyData(schemaUserUpdate), incidenteMiddleware, updateUserById);
+router.delete('/user/:id',verifyToken, verifyRole("Administrador"), incidenteMiddleware, deleteUserById);
 
 export default router;
