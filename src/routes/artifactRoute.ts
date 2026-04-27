981cab59a84dab731b7e53b052d7c2cea042f97c
JZHPX <176835358+JZHPX@users.noreply.github.com>
2026-04-26 20:25:01 -0500

    feat/ added middleware incidents.ts to GET artifacts route
    Se añadio el middleware de incidentes en la ruta Get de artefactos.

--- src/routes/artifactRoute.ts
+++ src/routes/artifactRoute.ts
@@ -9,7 +9,7 @@ import { incidenteMiddleware } from '../middlewares/incident.js';
 const router = Router();
 
 router.post("/artifacts", verifyToken, verifyRole("Administrador"),verifyData(createArtifactSchema), incidenteMiddleware, createArtifact);
-router.get("/artifacts", verifyToken, getArtifacts);
+router.get("/artifacts", verifyToken, incidenteMiddleware, getArtifacts);
 router.patch("/artifacts/:id", verifyToken, verifyRole("Administrador", "Gestor de proyectos", "Usuario"), verifyData(patchArtifactSchema), incidenteMiddleware, patchArtifacts);
 router.get("/artifacts/:id", verifyToken, incidenteMiddleware, getArtifactById); 
 router.delete("/artifacts/:id", verifyToken, verifyRole("Administrador"), incidenteMiddleware, deleteArtifact);
